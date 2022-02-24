43d27504dbf756ab68e740131417459c37735ed1
ConallMc <45179970+ConallMc@users.noreply.github.com>
2022-02-24 11:51:16 +0000

    Update Lab1.c
    Added MS DOS and comment formatting

--- Lab1.c
+++ Lab1.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 struct Partition{ char type; int start_sect; int size;} part_entry[4]; // 4 x partition table entry
 int main(int argc, char *argv[])
 {
@@ -22,6 +23,9 @@ fp = fopen(argv[1], "rb"); // Open file for reading - binary mode. Should use er
 fseek(fp, 0x1BE, SEEK_SET); // Seek to the start of the part_entry list
 fread(buf_part_table, 1, 64, fp); // Read the 64-byte block to memory buffer
 
+printf("############################## FORENSIC_TOOL_PHASE1 #################################### \n");
+printf("############################## Partitions on %s disk image ############################## \n", argv[1]);
+
 for (i=0; i <4; i++) {
 part_entry[i].type = *(char*)(buf_part_table + 0x04 +(i * offset) );
 if (part_entry[i].type == 0) not_exist++;
@@ -29,6 +33,7 @@ part_entry[i].start_sect = *(int*)(buf_part_table + 0x08 +(i * offset));
 part_entry[i].size = *(int*)(buf_part_table + 0x0C + (i * offset)) ;
  switch (part_entry[i].type) {
 case 00 : strcpy ( vol_type, "NOT-VALID"); break;
+case 05 : strcpy ( vol_type, "MS-DOS"); break;
 case 06 : strcpy ( vol_type, "FAT-16"); break;
 case 07 : strcpy ( vol_type, "NTFS"); break;
 case 0x0B: strcpy ( vol_type, "FAT-32"); break;
@@ -39,6 +44,8 @@ printf ("Partition %d: Type: %-12s Start: %-12d Size: %-12d\n", i, vol_type, par
 part_entry[i].size);
  }
 printf("\n\nThe total number of valid partitions is: %d\n\n", (4 - not_exist));
+printf("################################################################## \n");
+
 fclose(fp);
  return(0);
 }
